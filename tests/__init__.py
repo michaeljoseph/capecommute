bbb5799b196757cf87de60d02a668d7829bd0a05
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-06 16:50:48 +0200

    Update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -17,3 +17,18 @@ class TrainTestCase(TestCase):
         self.assertTrue(
             isinstance(train.parse_html(html), list)
         )
+
+    def test_pad_list(self):
+        row = [1, 2, 3]
+        self.assertEquals(
+            row + [None],
+            train.pad_list(row, 4)
+        )
+
+    def test_pad_list_right_size(self):
+        row = [1, 2, 3]
+        self.assertEquals(
+            row,
+            train.pad_list(row, 3)
+        )
+
